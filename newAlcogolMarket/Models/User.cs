fbdc9514903bac8c30f3e7c0689c21f59794d67c
ramenoj <tseren-ubushaev@mail.ru>
2022-05-15 01:49:38 +0300

    b

--- newAlcogolMarket/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace newAlcogolMarket.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-    }
-}
