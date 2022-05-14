eb0b2b1366c93cd8a99a87e484b6343705084bfb
Максим Козлов <Stud120101@stankin.ru>
2022-05-14 19:22:46 +0300

    сделал страницу корзины, привязал её к кнопке, заполнил страницу privacy,

--- newAlcogolMarket/Controllers/HomeController.cs
+++ newAlcogolMarket/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace newAlcogolMarket.Controllers
             return View();
         }
 
+        public IActionResult basket()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
