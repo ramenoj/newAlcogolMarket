aa92262c5de9fa300b5d1c7817f21e38b5ad8013
Максим Козлов <Stud120101@stankin.ru>
2022-05-14 23:59:08 +0300

    Добавил новые разделы привязал к ним кнопки

--- newAlcogolMarket/Controllers/HomeController.cs
+++ newAlcogolMarket/Controllers/HomeController.cs
@@ -28,6 +28,51 @@ namespace newAlcogolMarket.Controllers
             return View();
         }
 
+        public IActionResult Wine()
+        {
+            return View();
+        }
+
+        public IActionResult Liquor()
+        {
+            return View();
+        }
+
+        public IActionResult Champagne()
+        {
+            return View();
+        }
+
+        public IActionResult Whisky()
+        {
+            return View();
+        }
+
+        public IActionResult Cognac()
+        {
+            return View();
+        }
+
+        public IActionResult beer()
+        {
+            return View();
+        }
+
+        public IActionResult Absinthe()
+        {
+            return View();
+        }
+
+        public IActionResult Vodka()
+        {
+            return View();
+        }
+
+        public IActionResult Snacks()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
