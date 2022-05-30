0b4f38c84f79c69a2c11c6cb24f0c5844cf8db06
ramenoj <tseren-ubushaev@mail.ru>
2022-05-30 16:57:52 +0300

    fsdfsdf

--- newAlcogolMarket/Manager/Products/IProductManager.cs
+++ newAlcogolMarket/Manager/Products/IProductManager.cs
@@ -2,5 +2,12 @@
 {
     public interface IProductManager
     {
+        Task<List<Product>> GetAll();
+        Task Add(Product product);
+        Task Delete(int id);
+        Task Update(Product product);
+        Task<List<Product>> Filter(string name);
+        Product Get(Product product);
+        Task<List<Product>> GetAllByCategory(string categoryName);
     }
 }
